388ec3a8e740cfcc514f287f2e7c177eea31f5c4
ysu96 <55823937+ysu96@users.noreply.github.com>
2021-02-27 04:23:48 +0900

    Add files via upload

---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,30 @@
+package hello.core.order;
+
+import hello.core.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService;
+    OrderService orderService;
+    @BeforeEach //각 테스트를 실행하기 전에 호출
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+
+    @Test
+    void createOrder() {
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
