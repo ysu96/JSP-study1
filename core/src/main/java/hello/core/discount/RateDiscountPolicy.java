e6df5954e8808f763f647e11770d2a6a84728998
yu <ysu96@naver.com>
2021-03-02 01:42:32 +0900

    0302 commit

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,13 +2,21 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
-public class RateDiscountPolicy implements DiscountPolicy{
+@Component
+@Qualifier("mainDiscountPolicy")
+@Primary
+public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10; //10% 할인
+
     @Override
     public int discount(Member member, int price) {
-        if (member.getGrade() == Grade.VIP) {return price * discountPercent /
-                100;
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent /
+                    100;
         } else {
             return 0;
         }
